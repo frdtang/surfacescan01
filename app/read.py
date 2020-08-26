6c6cef8dfd3ca6fd3480b0efb8f6ab1270dfb23c
Francois Tang <francois.tang@hiptec.no>
2020-08-26 13:42:51 +0200

    added read test file

---
+++ app/read.py
@@ -0,0 +1,20 @@
+ 
+import time
+import serial
+ 
+read_port = serial.Serial(port='/dev/ttyS0',
+                          baudrate=38400,
+                          bytesize=8,
+                          stopbits=1,
+                          timeout=1)
+
+
+while True:
+    resp = read_port.readline()  
+    if resp:
+        print(resp)  
+    
+    #read_port.write(str.encode('Hello laptop!\n'))
+    time.sleep(1)
+          
+
