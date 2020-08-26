55e37082546486c97c34bcafe3668c2574270001
Francois Tang <francois.tang@hiptec.no>
2020-08-26 13:55:32 +0200

    added \rn

--- app/write.py
+++ app/write.py
@@ -3,14 +3,15 @@ import time
 import serial
  
 ser = serial.Serial(port='/dev/ttyS0',
-                          baudrate=38400,
-                          bytesize=8,
-                          stopbits=1,
-                          timeout=1)
+                    baudrate=38400,
+                    bytesize=8,
+                    stopbits=1,
+                    timeout=1)
 
+send_str = 'Hello laptop!\r\n'
 
 while True:
-    ser.write(str.encode('Hello laptop!\n'))
+    ser.write(str.encode(send_str))
     time.sleep(1)
           
 
