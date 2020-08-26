284360daf1e9a245b2edb82a9d9774c3d59b96e0
Francois Tang <francois.tang@hiptec.no>
2020-08-26 12:52:20 +0200

    encoding write

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -8,18 +8,13 @@ read_port = serial.Serial(port='/dev/ttyS0',
                           stopbits=1,
                           timeout=1)
 
-send_port = serial.Serial(port='/dev/ttyAMA0',
-                          baudrate=38400,
-                          bytesize=8,
-                          stopbits=1,
-                          timeout=1)
 
 while True:
     resp = read_port.readline()  
     if resp:
         print(resp)  
     
-    #send_port.write('Hello Laptop\n')
+    read_port.write(str.encode('Hello laptop!\n'))
     time.sleep(1)
           
 
