ab60db7ded870aa88e335b6bcad173a89384e78b
Francois Tang <francois.tang@hiptec.no>
2020-08-27 10:33:41 +0200

    update

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -1,16 +1,16 @@
  
 import time
-import serial.rs485
+import serial
  
-# port = serial.Serial(port='/dev/ttyAMA0',
-#                           baudrate=57600,
-#                           bytesize=8,
-#                           parity=serial.PARITY_EVEN,
-#                           stopbits=1)
+port = serial.Serial(port='/dev/ttyS0',
+                          baudrate=57600,
+                          bytesize=8,
+                          parity=serial.PARITY_EVEN,
+                          stopbits=1)
 
 
-port=serial.rs485.RS485(port='/dev/ttyS0',baudrate=57600)
-port.rs485_mode = serial.rs485.RS485Settings(False,True)
+# port=serial.rs485.RS485(port='/dev/ttyS0',baudrate=57600)
+# port.rs485_mode = serial.rs485.RS485Settings(False,True)
 
 
 port.write(b':01R010;0;E9C3r\n')
