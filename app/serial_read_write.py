7d3e235b528d50957d9eca7b2888bbf5a05abd92
Francois Tang <francois.tang@hiptec.no>
2020-08-27 13:38:25 +0200

    unicode

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -14,7 +14,7 @@ port = serial.Serial(port='/dev/ttyS0',
 port.write(b':01R010;0;E9C3r\n')
 while True:
 
-    port.write(str.encode(':01R021;09F4r\n'))
+    port.write(str.encode(':01R021;09F4r\n'.encode('utf-8')))
     resp = port.readline()  
     if resp:
         print(resp)  
