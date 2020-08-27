4283728e1d09ebc5b9af8b62cc8a086dc780c2d7
Francois Tang <francois.tang@hiptec.no>
2020-08-27 13:39:33 +0200

    bug

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -11,7 +11,7 @@ port = serial.Serial(port='/dev/ttyS0',
 
    
 
-port.write(b':01R010;0;E9C3r\n'.encode('utf-8'))
+port.write(':01R010;0;E9C3r\n'.encode('utf-8'))
 while True:
 
     port.write(':01R021;09F4r\n'.encode('utf-8'))
