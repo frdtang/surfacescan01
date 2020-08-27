3804150f4d5f0b0749f29c17973484075b7af5da
Francois Tang <francois.tang@hiptec.no>
2020-08-27 11:28:28 +0200

    update

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -3,9 +3,6 @@ import time
 import serial
  
  
-import time
-import serial
- 
 port = serial.Serial(port='/dev/ttyS0',
                           baudrate=57600,
                           bytesize=8,
@@ -17,7 +14,7 @@ port = serial.Serial(port='/dev/ttyS0',
 port.write(b':01R010;0;E9C3r\n')
 while True:
 
-    port.write(b':01R021;09F4r\n')
+    port.write(str.encode(':01R021;09F4r\n'))
     resp = port.readline()  
     if resp:
         print(resp)  
