db75c6472bfcba09f07ae7cb154d5c6db80fbf06
Francois Tang <francois.tang@hiptec.no>
2020-08-28 09:45:55 +0200

    update

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -2,16 +2,12 @@
 import time
 import serial
  
- 
 port = serial.Serial(port='/dev/ttyS0',
                           baudrate=57600,
                           bytesize=8,
                           stopbits=1,
                           timeout=1
                           )
-
-   
-
 
 while True:
     port.write(b'Hello world')
