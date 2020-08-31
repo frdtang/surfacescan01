a1b5dda66f05abd174d3dbf1deca17c88ab2da91
Francois Tang <francois.tang@hiptec.no>
2020-08-31 09:42:51 +0200

    test

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -7,9 +7,6 @@ import binascii
  
 read_port = serial.Serial(port='/dev/ttyS0',
                           baudrate=57600,
-                          bytesize=8,
-                          stopbits=1,
-                          timeout=1
                           )
 
 
@@ -18,15 +15,13 @@ write_port = pyftdi.serialext.serial_for_url('ftdi://ftdi:232:FT4IVQEG/1',
                                         baudrate=57600,
                                         bytesize=8,
                                         parity=serial.PARITY_EVEN,
-                                        stopbits=1,                            
-                                        timeout=1)
+                                        stopbits=1)
 
 Ftdi.show_devices()
 
 
 send_string = b":01W010;0;E9C3\r\n"
 time.sleep(1)
-send_string = binascii.hexlify(b":01W010;0;E9C3\r\n")
 write_port.write(send_string)
 resp = read_port.readline()  
 if resp:
@@ -35,7 +30,6 @@ if resp:
 
 send_string = b":01R002;3955\r\n"
 time.sleep(1)
-send_string = binascii.hexlify(b":01R002;3955\r\n")
 write_port.write(send_string)
 resp = read_port.readline()  
 if resp:
