1efe2931d5d74967256072a1bbb1709a0f740307
Francois Tang <francois.tang@hiptec.no>
2020-08-31 08:36:30 +0200

    created write port

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -2,14 +2,28 @@
 import time
 import serial
  
-port = serial.Serial(port='/dev/ttyS0',
+read_port = serial.Serial(port='/dev/ttyS0',
                           baudrate=57600,
                           bytesize=8,
                           stopbits=1,
                           timeout=1
                           )
 
+
+
+write_port = pyftdi.serialext.serial_for_url('ftdi://ftdi:232:FT4IVQEG/1',
+                                        baudrate=57600,
+                                        bytesize=8,
+                                        parity='E',
+                                        stopbits=1,                            
+                                        timeout=1)
+
 while True:
-    resp = port.readline()  
+    send_string = ":01R021;09F4\r\n"
+    write_port.write(str.encode(send_string))
+    
+    time.sleep(1)
+    
+    resp = read_port.readline()  
     if resp:
         print(resp)  
