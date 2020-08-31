0c7d80fe64f8f4598682220053d4cafd53d2d856
Francois Tang <francois.tang@hiptec.no>
2020-08-31 15:36:01 +0200

    added measure.py

---
+++ app/measure.py
@@ -0,0 +1,52 @@
+ 
+import time
+import serial
+import pyftdi.serialext
+from pyftdi.ftdi import Ftdi
+import binascii
+ 
+read_port = serial.Serial(port='/dev/ttyS0',
+                          baudrate=38400,
+                          bytesize=8,
+                          stopbits=1,
+                          timeout=1
+                          )
+
+
+write_port = pyftdi.serialext.serial_for_url('ftdi://ftdi:232:FT4IVQEG/1',
+                                        baudrate=38400,
+                                        bytesize=8,
+                                        parity=serial.PARITY_EVEN,
+                                        stopbits=1,
+                                        timeout=1)
+
+Ftdi.show_devices()
+
+
+send_string = b":01W010;0;E9C3\r\n"
+write_port.write(send_string)
+
+resp = True
+while resp:
+	resp = read_port.readline()  
+	resp = read_port.readline()  
+
+
+send_string = b":01R002;3955\r\n"
+write_port.write(send_string)
+resp = read_port.readline()  
+if resp:
+    print(resp)  
+
+time.sleep(1)
+resp = read_port.readline()  
+if resp:
+    print(resp) 
+
+while True:
+    send_string = b":01R021;****\r\n"
+    write_port.write(send_string)    
+    resp = read_port.readline()  
+    if resp:
+        print(resp)  
+
