f9d0f09d807dc2275eecdafeda85fa99de21494c
Francois Tang <francois.tang@hiptec.no>
2020-08-31 10:44:43 +0200

    change bandrate

--- app/serial_read_write.py
+++ app/serial_read_write.py
@@ -6,16 +6,15 @@ from pyftdi.ftdi import Ftdi
 import binascii
  
 read_port = serial.Serial(port='/dev/ttyS0',
-                          baudrate=57600,
+                          baudrate=38400,
                           bytesize=8,
                           stopbits=1,
                           timeout=1
                           )
 
 
-
 write_port = pyftdi.serialext.serial_for_url('ftdi://ftdi:232:FT4IVQEG/1',
-                                        baudrate=57600,
+                                        baudrate=38400,
                                         bytesize=8,
                                         parity=serial.PARITY_EVEN,
                                         stopbits=1,
@@ -24,18 +23,18 @@ write_port = pyftdi.serialext.serial_for_url('ftdi://ftdi:232:FT4IVQEG/1',
 Ftdi.show_devices()
 
 
-send_string = ":01W010;0;E9C3\r\n"
+send_string = b":01W010;0;E9C3\r\n"
 time.sleep(1)
-write_port.write(send_string.encode('ascii'))
+write_port.write(b":01W010;0;E9C3\r\n")
 
 resp = read_port.readline()  
 if resp:
     print(resp)  
 
 
-send_string = ":01R002;3955\r\n"
+send_string = b":01R002;3955\r\n"
 time.sleep(1)
-write_port.write(send_string.encode('ascii'))
+write_port.write(send_string)
 
 resp = read_port.readline()  
 if resp:
