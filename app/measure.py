6616c62f587c56437e8b58782db79898fdee3d51
Francois Tang <francois.tang@hiptec.no>
2020-09-01 14:49:10 +0200

    1000 points

--- app/measure.py
+++ app/measure.py
@@ -70,7 +70,7 @@ class Disk_Surface():
                        "q": 0,
                        "time": 0}
         
-        while count<100:
+        while count<1000:
             self._write_port.write(":01R021;****\r\n")    
             resp = self._read_port.read(35)  
             time_now = round(time.time()-start_time,3)
