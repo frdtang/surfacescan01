b247bd876bdf4c7229fdaba186af698da472b9ce
Francois Tang <francois.tang@hiptec.no>
2020-10-09 12:17:10 +0200

    dumping results

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from app.measure import Disk_Surface
+import json
 app = Flask(__name__)
  
 @app.route('/distance', methods = ["GET"])
@@ -8,7 +9,7 @@ def distance():
     '''
     disk_surface = Disk_Surface()
     disk_surface.measure()    
-    return disk_surface.distance,\
+    return json.dumps(disk_surface.distance),\
         200, {'ContentType': 'application/json'}
 
 @app.route('/', methods = ["GET"])
